b8cd48d37a40bbed3cd79881190ab7e1233fc6d0
wizard <15109929502@163.com>
2019-09-02 10:07:15 +0800

    1.2.2wizard社区登录模块

--- src/main/java/lift/wizard/community/controller/AuthorizeController.java
+++ src/main/java/lift/wizard/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import lift.wizard.community.dto.AccessTokenDTO;
 import lift.wizard.community.dto.GithubUser;
 import lift.wizard.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,18 +14,28 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("6a0f54825dc9de1dfecd");
-        accessTokenDTO.setClient_secret("6fcf00383f05845c76fac5639cbaa878c2176254");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8886/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.gitUser(accessToken);
         System.out.println(user.getName());
+        System.out.println(user.getId());
 
         return "index";
     }
